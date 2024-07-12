3b142e3cab6f73e32a17579e5149027e82f47e6f
Thairy Daza <thai@mac.local>
2024-07-12 18:03:56 -0300

    adding terraform code for lambda creation

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,3 @@
-# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
-# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
 
 # Create Lambda function
 resource "aws_lambda_function" "cloud_resume" {
